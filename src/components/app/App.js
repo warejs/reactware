240d86436196cf0f751709bb0745157dc8a0aa5d
Cezarijus Kivylius <cezarijus.kivylius@servisbot.com>
2018-12-21 03:28:28 +0000

    Revert "Feature/ui other (#4)" (#6)
    This reverts commit 58a5ea146564acc038c446706d57c96f7aab2695.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -44,13 +44,8 @@ const BodyStyled = styled.div`
 `;
 
 const SidebarStyled = styled.div`
-<<<<<<< HEAD
-  -background: #535353;
-  -padding: 3px;
-=======
   background: #535353;
   padding: 3px;
->>>>>>> master
 `;
 
 const LeftStyled = styled.div`
@@ -93,25 +88,11 @@ export default class App extends React.Component {
   static propTypes = {
     menus: PropTypes.object,
     windows: PropTypes.array,
-<<<<<<< HEAD
-    containers: PropTypes.object,
-=======
->>>>>>> master
   };
 
   static defaultProps = {
     menus: {},
     windows: [],
-<<<<<<< HEAD
-    menu: [],
-    containers: {
-      right: [],
-      bottom: [],
-      left: [],
-      top: [],
-    }
-=======
->>>>>>> master
   };
 
   constructor(props) {
